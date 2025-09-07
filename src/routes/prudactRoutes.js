4b156ca507d518695c8b17d786430e0119855023
mhammd676 <mhammd6mahmoud@gmail.com>
2025-09-08 01:40:55 +0300

    Update prudactRoutes.js

--- src/routes/prudactRoutes.js
+++ src/routes/prudactRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { authMiddleware } = require("../middlewares/auth.middlewares");
 const {
   getProducts,
   getProductById
@@ -7,9 +8,9 @@ const {
 
 
 
-router.get('/', getProducts);
+router.get('/',authMiddleware, getProducts);
 
 
-router.get("/:id",getProductById)
+router.get("/:id",authMiddleware,getProductById)
 
 module.exports = router;
