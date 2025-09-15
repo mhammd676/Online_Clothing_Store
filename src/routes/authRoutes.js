69421396d3b1b3bba928e26c4f276571d28a41b1
mhammd676 <mhammd6mahmoud@gmail.com>
2025-09-15 22:01:35 +0300

    Update auth routes and dependencies

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {registerUser, login } = require("../controllers/authController");
-
+const { authMiddleware } = require("../middlewares/auth.middlewares");
 const router = express.Router();
 
 // register user
-router.post("/register", registerUser);
+router.post("/register", authMiddleware,registerUser);
 
 // login user
-router.post("/login", login);
+router.post("/login",authMiddleware, login);
 
 module.exports = router;
